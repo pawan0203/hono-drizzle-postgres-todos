81a19d6695f764560358b84a2ec7c94c390f660e
pawan0203 <pawanmaharwal0203@gmail.com>
2026-05-19 13:23:02 +0530

    feat: add user and todos tables to database schema

---
+++ src/db/schema.ts
@@ -0,0 +1,38 @@
+import { sql } from "drizzle-orm";
+import {
+  boolean,
+  check,
+  integer,
+  pgTable,
+  timestamp,
+  uuid,
+  varchar,
+} from "drizzle-orm/pg-core";
+
+export const userTable = pgTable(
+  "users",
+  {
+    id: uuid().primaryKey().defaultRandom(),
+    email: varchar({ length: 256 }).unique().notNull(),
+    hashPassword: varchar({ length: 400 }).notNull(),
+    age: integer(),
+    createdAt: timestamp({ withTimezone: true }).defaultNow(),
+    updatedAt: timestamp({ withTimezone: true }).defaultNow(),
+  },
+  (table) => [
+    check("age_check", sql`${table.age}<=120`),
+    check("age_check2", sql`${table.age}>=0`),
+  ],
+);
+
+export const todosTable = pgTable("todos", {
+  id: uuid().primaryKey().defaultRandom(),
+  userId: uuid()
+    .notNull()
+    .references(() => userTable.id, { onDelete: "cascade" }),
+  title: varchar({ length: 500 }).notNull(),
+  description: varchar({ length: 1000 }),
+  completed: boolean().default(false),
+  createdAt: timestamp({ withTimezone: true }).defaultNow(),
+  updatedAt: timestamp({ withTimezone: true }).defaultNow(),
+});
